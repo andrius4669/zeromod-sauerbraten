4cc91c4eb6b316c368aa78ee69d057d524548a5e
andrius4669 <andrius4669@gmail.com>
2015-01-15 02:07:36 +0200

    add missing z_bans.h

---
+++ src/fpsgame/z_bans.h
@@ -0,0 +1,46 @@
+#ifndef Z_BANS_H
+#define Z_BANS_H
+
+void z_teamkillspectate(uint ip)
+{
+    loopv(clients) if(clients[i]->state.aitype==AI_NONE && clients[i]->state.state!=CS_SPECTATOR && getclientip(clients[i]->clientnum)==ip)
+    {
+        extern void forcespectator(clientinfo *);
+        forcespectator(clients[i]);
+        sendf(clients[i]->clientnum, 1, "ris", N_SERVMSG, "you got spectated because teamkills limit was reached");
+    }
+}
+
+bool z_checkban(uint ip)
+{
+    loopv(bannedips) if(bannedips[i].ip==ip) switch(bannedips[i].type)
+    {
+        case BAN_MUTE: case BAN_SPECTATE: continue;
+        case BAN_TEAMKILL:
+            if(teamkillspectate) continue;
+            // FALL THROUGH
+        default: return true;
+    }
+    return false;
+}
+
+bool z_applyspecban(clientinfo *ci)
+{
+    if(ci->local) return false;
+    uint ip = getclientip(ci->clientnum);
+    loopv(bannedips) if(bannedips[i].ip==ip && (bannedips[i].type==BAN_SPECTATE || (teamkillspectate && bannedips[i].type==BAN_TEAMKILL)))
+    {
+        ban &b = bannedips[i];
+        sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("you are spectated because of %s ban", b.type==BAN_TEAMKILL ? "teamkills" : "spectate"));
+        return true;
+    }
+    return false;
+}
+
+bool z_checkmuteban(uint ip)
+{
+    loopv(bannedips) if(bannedips[i].ip==ip && bannedips[i].type==BAN_MUTE) return true;
+    return false;
+}
+
+#endif // Z_BANS_H
