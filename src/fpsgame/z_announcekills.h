c46e3d7022e5fb387f2981ce1e5d30729cd7bae0
andrius4669 <andrius4669@gmail.com>
2015-03-09 02:43:45 +0200

    use string templates for announcekills messages

--- src/fpsgame/z_announcekills.h
+++ src/fpsgame/z_announcekills.h
@@ -4,9 +4,10 @@
 
 VAR(announcekills, 0, 0, 1);
 
+VAR(announcekills_teamnames, 0, 1, 1);
+
 VAR(announcekills_stopped, 0, 1, 1);
 VAR(announcekills_stopped_min, 0, 15, 100);
-VAR(announcekills_stopped_num, 0, 1, 1);
 
 VAR(announcekills_multikill, 0, 2, 2);
 
@@ -15,12 +16,46 @@ VAR(announcekills_rampage_min, 0, 0, 100);
 VAR(announcekills_numeric, 0, 0, 1);
 VAR(announcekills_numeric_num, 1, 5, 100);
 
+
+struct z_multikillstrstruct { int val; char *str; };
+vector<z_multikillstrstruct> z_multikillstrings;
+ICOMMAND(announcekills_multikillstrings, "is2V", (tagval *args, int numargs),
+{
+    loopv(z_multikillstrings) delete[] z_multikillstrings[i].str;
+    z_multikillstrings.setsize(0);
+    for(int i = 0; i + 1 < numargs; i += 2)
+    {
+        z_multikillstrstruct &v = z_multikillstrings.add();
+        v.val = args[i].getint();
+        v.str = newstring(args[i+1].getstr());
+    }
+});
+
+SVAR(announcekills_style_stopped, "\f2%V was stopped by %A (\f6%n kills\f2)");
+SVAR(announcekills_style_multikills, "\f2%Y scored %sKILL!!");
+SVAR(announcekills_style_multikills_num, "\f2%Y scored \f3%n MULTIPLE KILLS!!");
+SVAR(announcekills_style_numeric, "\f2%Y made \f7%n\f2 kills");
+SVAR(announcekills_style_rampage, "\f2%s is %s");
+
+vector<z_multikillstrstruct> z_rampagestrings;
+ICOMMAND(announcekills_rampagestrings, "is2V", (tagval *args, int numargs),
+{
+    loopv(z_rampagestrings) delete[] z_rampagestrings[i].str;
+    z_rampagestrings.setsize(0);
+    for(int i = 0; i + 1 < numargs; i += 2)
+    {
+        z_multikillstrstruct &v = z_rampagestrings.add();
+        v.val = args[i].getint();
+        v.str = newstring(args[i+1].getstr());
+    }
+});
+
 static const char *z_teamcolorname(clientinfo *ci, const char *alt, clientinfo *as)
 {
     static string buf[3];
     static int bufpos = 0;
     const char *name = alt && ci==as ? colorname(ci, alt) : colorname(ci);
-    if(!m_teammode) return name;
+    if(!m_teammode || !announcekills_teamnames) return name;
     bufpos = (bufpos+1)%3;
     formatstring(buf[bufpos])("\fs\f%c%s\fr", !strcmp(ci->team, as->team) ? '1' : '3', name);
     return buf[bufpos];
@@ -28,6 +63,11 @@ static const char *z_teamcolorname(clientinfo *ci, const char *alt, clientinfo *
 
 static const char *z_multikillstr(int i)
 {
+    if(z_multikillstrings.length())
+    {
+        loopv(z_multikillstrings) if(z_multikillstrings[i].val == i) return z_multikillstrings[i].str;
+        return NULL;
+    }
     switch(i)
     {
         case 2: return "a \f6DOUBLE ";
@@ -63,19 +103,39 @@ void z_announcekill(clientinfo *actor, clientinfo *victim, int fragval)
         {
             const char *vname = z_teamcolorname(victim, NULL, ci);
             nname = z_teamcolorname(actor, NULL, ci);
-            /* TODO: use templates */
-            if(!announcekills_stopped_num) sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s was stopped by %s", vname, nname));
-            else sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s was stopped by %s (\f6%d kills\f2)", vname, nname, victim->state.rampage));
+
+            z_formattemplate ft[] =
+            {
+                { 'V', "%s", (const void *)vname },
+                { 'A', "%d", (const void *)nname },
+                { 'n', "%d", (const void *)(long)victim->state.rampage },
+                { 0, NULL, NULL }
+            };
+
+            string buf;
+            z_format(buf, sizeof buf, announcekills_style_stopped, ft);
+            if(*buf) sendf(ci->clientnum, 1, "ris", N_SERVMSG, buf);
         }
 
         if(showactor && announcekills_multikill && actor->state.multikills > 1 &&
             (announcekills_multikill == 1 || ci->clientnum == actor->clientnum))
         {
             name = z_teamcolorname(actor, "you", ci);
+            if(!nname) nname = z_teamcolorname(actor, NULL, ci);
             const char *mkstr = z_multikillstr(actor->state.multikills);
-            /* TODO: use templates */
-            if(mkstr) sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s scored %sKILL!!", name, mkstr));
-            else sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s scored \f3%d MULTIPLE KILLS!!", name, actor->state.multikills));
+
+            z_formattemplate ft[] =
+            {
+                { 'Y', "%s", (const void *)name },
+                { 'A', "%d", (const void *)nname },
+                { 's', "%s", (const void *)mkstr },
+                { 'n', "%d", (const void *)(long)actor->state.multikills },
+                { 0, NULL, NULL }
+            };
+
+            string buf;
+            z_format(buf, sizeof buf, mkstr ? announcekills_style_multikills : announcekills_style_multikills_num, ft);
+            if(*buf) sendf(ci->clientnum, 1, "ris", N_SERVMSG, buf);
         }
 
         bool shouldbroadcast = announcekills_rampage == 1 && actor->state.rampage >= announcekills_rampage_min;
@@ -87,27 +147,57 @@ void z_announcekill(clientinfo *actor, clientinfo *victim, int fragval)
                 if(actor->state.rampage % announcekills_numeric_num == 0)
                 {
                     if(!name) name = z_teamcolorname(actor, "you", ci);
-                    /* TODO: use templates */
-                    sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s made \f7%d\f2 kills", name, actor->state.rampage));
+                    if(!nname) nname = z_teamcolorname(actor, NULL, ci);
+
+                    z_formattemplate ft[] =
+                    {
+                        { 'Y', "%s", (const void *)name },
+                        { 'A', "%d", (const void *)nname },
+                        { 'n', "%d", (const void *)(long)actor->state.rampage },
+                        { 0, NULL, NULL }
+                    };
+
+                    string buf;
+                    z_format(buf, sizeof buf, announcekills_style_numeric, ft);
+                    if(*buf) sendf(ci->clientnum, 1, "ris", N_SERVMSG, buf);
                 }
             }
             else
             {
                 const char *msg;
-                /* TODO: use admin supplied list */
-                switch(actor->state.rampage)
+                if(z_rampagestrings.length())
                 {
-                    case 5:  msg = "on a \f6KILLING SPREE!!"; break;
-                    case 10: msg = "on a \f6RAMPAGE!!"; break;
-                    case 15: msg = "\f6DOMINATING!!"; break;
-                    case 20: msg = "\f6UNSTOPPABLE!!"; break;
-                    case 30: msg = "\f6GODLIKE!!"; break;
-                    default: msg = NULL; break;
+                    msg = NULL;
+                    loopv(z_rampagestrings) if(z_rampagestrings[i].val == actor->state.rampage) { msg = z_rampagestrings[i].str; break; }
+                }
+                else
+                {
+                    switch(actor->state.rampage)
+                    {
+                        case 5:  msg = "on a \f6KILLING SPREE!!"; break;
+                        case 10: msg = "on a \f6RAMPAGE!!"; break;
+                        case 15: msg = "\f6DOMINATING!!"; break;
+                        case 20: msg = "\f6UNSTOPPABLE!!"; break;
+                        case 30: msg = "\f6GODLIKE!!"; break;
+                        default: msg = NULL; break;
+                    }
                 }
                 if(msg)
                 {
+                    if(!name) name = z_teamcolorname(actor, "you", ci);
                     if(!nname) nname = z_teamcolorname(actor, NULL, ci);
-                    sendf(ci->clientnum, 1, "ris", N_SERVMSG, tempformatstring("\f2%s is %s", nname, msg));
+
+                    z_formattemplate ft[] =
+                    {
+                        { 'Y', "%s", (const void *)name },
+                        { 'A', "%d", (const void *)nname },
+                        { 's', "%d", (const void *)msg },
+                        { 0, NULL, NULL }
+                    };
+
+                    string buf;
+                    z_format(buf, sizeof buf, announcekills_style_rampage, ft);
+                    if(*buf) sendf(ci->clientnum, 1, "ris", N_SERVMSG, buf);
                 }
             }
         }
