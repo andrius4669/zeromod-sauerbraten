51a733b33c7f91694eb398efe5fea730999d1eb1
andrius4669 <andrius4669@gmail.com>
2015-03-15 14:05:26 +0200

    workaround for geoip detection failures on ubuntu systems

--- src/check_geoip.sh
+++ src/check_geoip.sh
@@ -2,14 +2,14 @@
 
 CXX=$*
 
-$CXX -o /dev/null -x c++ '-lGeoIP' '-' 2>/dev/null << EOF
+$CXX -o /dev/null -x c++ - -lGeoIP 2>/dev/null << EOF
 #include "GeoIP.h"
 #include "GeoIPCity.h"
 int main() { GeoIP *gi = NULL; GeoIP_delete(gi); return 0; }
 EOF
 if [ $? -eq 0 ]; then printf ' -DUSE_GEOIP'; fi
 
-$CXX -o /dev/null -x c++ '-lmaxminddb' '-' 2>/dev/null << EOF
+$CXX -o /dev/null -x c++ - -lmaxminddb 2>/dev/null << EOF
 #include "maxminddb.h"
 int main() { MMDB_s *m = NULL; MMDB_close(m); return 0; }
 EOF
