2f23ccffcd1fbc0cc296678a07d64be1bc3d845d
andrius4669 <andrius4669@gmail.com>
2018-01-14 22:42:22 +0200

    sync w/ upstream

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -3544,6 +3544,7 @@ namespace server
             {
                 getstring(text, p);
                 filtertext(text, text, false);
+                fixmapname(text);
                 int reqmode = getint(p);
                 vote(text, reqmode, sender);
                 break;
