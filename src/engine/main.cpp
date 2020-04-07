bf3863fba41bfd91f56a69cde124cdaebd97b626
andrius4669 <andrius4669@gmail.com>
2020-04-07 23:10:38 +0300

    comment out debug stuff

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -461,9 +461,11 @@ bool shouldgrab = false, grabinput = false, minimized = false, canrelativemouse 
 
 void inputgrab(bool on)
 {
+#if 0
     logoutf(
         "input grab on:%d canrelativemouse:%d userelativemouse:%d",
         int(on), int(canrelativemouse), int(userelativemouse));
+#endif
     if(on)
     {
         SDL_ShowCursor(SDL_FALSE);
@@ -472,14 +474,18 @@ void inputgrab(bool on)
             if(SDL_SetRelativeMouseMode(SDL_TRUE) >= 0)
             {
                 SDL_SetWindowGrab(screen, SDL_TRUE);
+#if 0
                 SDL_bool g = SDL_GetWindowGrab(screen);
                 if (!g) logoutf(
                     "input grab failed: not grabbed");
+#endif
                 relativemouse = true;
             }
             else
             {
+#if 0
                 logoutf("input grab failed set relative");
+#endif
                 SDL_SetWindowGrab(screen, SDL_FALSE);
                 canrelativemouse = false;
                 relativemouse = false;
