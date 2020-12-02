9b488b5ff4cb0744c7afe72d86f98cd8bc19ec15
andrius4669 <andrius4669@gmail.com>
2020-12-02 17:51:02 +0000

    tweak sync stuff

--- src/svn-up.sh
+++ src/svn-up.sh
@@ -8,5 +8,19 @@ then
 	exit 1
 fi
 
-svn up "$@"
-svn info --show-item revision > svnrev.txt
+svnrev=$(svn info --show-item last-changed-revision)
+gitrev=$(cat svnrev.txt)
+
+if [ $svnrev != $gitrev ]
+then
+	if [ $svnrev -lt $gitrev ]
+	then
+		svn up -r $gitrev --accept mine
+	else
+		echo "error: local repo is at higher revision than git one" >&2
+		exit 1
+	fi
+fi
+
+svn up
+svn info --show-item last-changed-revision > svnrev.txt
