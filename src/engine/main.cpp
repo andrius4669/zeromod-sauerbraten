7aff6fbb4da790b67b5f32cb2f0b2f6d5069bacc
andrius4669 <andrius4669@gmail.com>
2020-12-02 13:39:56 +0000

    fix color depth for myself

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -671,6 +671,9 @@ void setupscreen()
 
     SDL_GL_ResetAttributes();
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
     static const int configs[] =
     {
         0x3, /* try everything */
